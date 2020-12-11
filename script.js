ae2f298f02c63980246f72c0b6637501677829e8
Jacqueline Rivera <Jacqueline8996@gmail.com>
2020-12-11 01:28:45 -0500

    added number generator

--- script.js
+++ script.js
@@ -4,38 +4,45 @@ var generateBtn = document.querySelector("#generate");
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var symbol = ['','!', '"','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>', '?','@','[', '\\', ']','^','_','`','{','|','}','~'];
+var smallLength = 8
+var largeLength = 128
 
 //function for prompt 
-
 function userInput(){
 
-    //Prompts for the User to answer prefrence on the pass word 
-
     var passwordLength = prompt("How Long do you want your password? (Please be between 8 and 128 characters)");
-    //checking if password is between 8 and 128 characters
-    if (passwordLength >= 8 || passwordLength <=128){
-    //ask if lower case /ask if there is uppper cases (if statement) nedd to validate data make data validation function)
-    var lowerCase = prompt("Do you want lowerCase? (yes/no)");
+   
+    //verifying it is within the paramaters  
+
+    for (passwordLength < smallLength ;passwordLength > largeLength;){
+        //does not work with min 
+        if (passwordLength < smallLength){
+            alert("Password is too short , try again!!!!");
+            
+        }
+        else{
+            alert("password is too long , try again!!!!"); 
+        }
+        var passwordLength = prompt("How Long do you want your password? (Please be between 8 and 128 characters)");
+    }
+
+    //ask if lower case /ask if there is uppper cases (if statement) nedd to validate data make data validation function
+    var lowerCase = confirm("Do you want lowerCase?");
+
     //ask if uppercase
-    var UpperCase = prompt("Do you want UpperCase?(yes/no)");
+    var UpperCase = confirm("Do you want UpperCase?");
     //ask if they want numbers 
-    var numberChar = prompt("Do you want numbers?(yes/no)");
+    var numberChar = confirm("Do you want numbers?");
     //ask for special charcters 
-    var specialChar = prompt("Do you want Special Characters?(yes/no)");
+    var specialChar = confirm("Do you want Special Characters?");
 }
 
-else{
-    if (passwordLength <= 8){
-        alert("Password is too short , try again!!!!");
-    }
-    else{
-        alert("password is too long , try again!!!!")
-    }
+//function fo number generator
+function numberGen(){
+    var num = Math.floor(Math.random() * (Array.length))
 }
 
-}
 
-//function fo number generato
 
 
 //Code to generate the password from input 
@@ -43,7 +50,7 @@ else{
 // Write password to the #password input
 function writePassword() {
 
-    userInput()
+    userInput();
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
@@ -53,5 +60,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
